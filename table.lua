dd9049addbdebe4a2b2b7e1fdc2cbd1b3297d9a5
HysenChan <HysenChan@163.com>
2020-08-25 16:31:51 +0800

    【添加】区分a.x和a["x"]和a[x]，其中a.x==a["x"]，a[x]是唯一的

--- table.lua
+++ table.lua
@@ -8,13 +8,29 @@
 -- a["x"]=a["x"]+5 --递增条目"x"
 -- print(a["x"])   --15
 
-a={}
-a["x"]=10
-b=a --b与a引用了同一个table
-print(b["x"])   --10
-b["x"]=20
-print(a["x"])   --20
-a=nil   --现在只有b还在引用table
-print(b["x"]+2) --22
-b=nil   --b也没有table的引用了
+-- a={}
+-- a["x"]=10
+-- b=a --b与a引用了同一个table
+-- print(b["x"])   --10
+-- b["x"]=20
+-- print(a["x"])   --20
+-- a=nil   --现在只有b还在引用table
+-- print(b["x"]+2) --22
+-- b=nil   --b也没有table的引用了
 
+a={}
+for i = 1, 1000 do
+    a[i]=i*2
+end
+print(#a)   --1000
+print(a[9])
+a["x"]=10
+print(a["x"])   --10  ==print(a.x)
+print(a["y"])   --nil  ==print(a.y)
+
+b={}
+x="y"   --a[x]=10
+a[x]=10 --把10放入字段"y"   ==a["y"]=10
+print(a[x]) --a["y"]
+print(a.x)  --a["x"]=nil
+print(a.y)  --a["y"]
