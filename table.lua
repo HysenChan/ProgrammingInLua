3217d1aa0212aef9106d77309c09bef78cc96d78
HysenChan <HysenChan@163.com>
2020-08-25 16:55:58 +0800

    【添加】'#'长度操作符的使用

--- table.lua
+++ table.lua
@@ -18,19 +18,42 @@
 -- print(b["x"]+2) --22
 -- b=nil   --b也没有table的引用了
 
-a={}
-for i = 1, 1000 do
-    a[i]=i*2
-end
-print(#a)   --1000
-print(a[9])
-a["x"]=10
-print(a["x"])   --10  ==print(a.x)
-print(a["y"])   --nil  ==print(a.y)
+-- a={}
+-- for i = 1, 1000 do
+--     a[i]=i*2
+-- end
+-- print(#a)   --1000
+-- print(a[9])
+-- a["x"]=10
+-- print(a["x"])   --10  ==print(a.x)
+-- print(a["y"])   --nil  ==print(a.y)
 
+-- b={}
+-- x="y"   --a[x]=10
+-- a[x]=10 --把10放入字段"y"   ==a["y"]=10
+-- print(a[x]) --a["y"]
+-- print(a.x)  --a["x"]=nil
+-- print(a.y)  --a["y"]
+
+--读取10行内容，并存储到一个table中
+a={}
+for i = 1, 10 do
+    a[i]=io.read()
+end
+
+--#a来获取到a的长度，打印所有的行
+for i = 1, #a do
+    print(a[i])
+end
+
+print(a[#a])    --打印列表a的最后一个值
+a[#a]=nil   --删除最后一个值
+a[#a+1]=v   --将v添加到列表末尾
+print(a[#a+1])
+
+--另一种方法读取一个文件的前10行
 b={}
-x="y"   --a[x]=10
-a[x]=10 --把10放入字段"y"   ==a["y"]=10
-print(a[x]) --a["y"]
-print(a.x)  --a["x"]=nil
-print(a.y)  --a["y"]
+for i = 1, 10 do
+    b[#b+1] = io.read();    --#b是因为开始时b的长度为0，所以[#b+1]与上面a[1]是一致的
+end
+
