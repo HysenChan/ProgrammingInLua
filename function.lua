a2d003b9b9fbabe44ad7b793b3719144bacf1802
HysenChan <HysenChan@163.com>
2020-08-26 14:25:54 +0800

    【添加】【函数】添加函数使用时，隐藏的技巧

---
+++ function.lua
@@ -0,0 +1,15 @@
+--第五章——函数
+print(8*9,9/8)
+a=math.sin(3)+math.cos(10)
+print(a)
+print(os.date())
+
+--一个函数只有一个参数，并且参数是一个字面字符串或table构造式，则圆括号可有可无
+print "Hello World" --Hello World
+print [[a multi-line message]]  --a multi-line message
+type{}
+
+--冒号操作符
+--调用o.foo时将o隐含地作为函数的第一个参数
+o.foo(o,x)  --> ==o:foo(x)
+
