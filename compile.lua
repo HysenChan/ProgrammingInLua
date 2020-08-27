6218a38b4b65dbc51640c650e1dab09d119ad285
HysenChan <HysenChan@163.com>
2020-08-27 12:56:59 +0800

    【添加】[编译]完成编译和定义两者的使用问题

--- compile.lua
+++ compile.lua
@@ -16,3 +16,24 @@ end
 
 f() --2 操作全局i
 g() --1 操作局部i
+
+-- print "Enter your expression:"
+-- local l=io.read()
+-- local func=assert(loadstring("return " .. l))
+-- print("the value of your expression is " .. func())
+
+-- print "enter funciton to be plotted (with variable 'x'):"
+-- local l=io.read()
+-- local f=assert(loadstring("local x=...;return " .. l))
+-- for i = 1, 20 do
+--     print(string.rep("*",f()))  --string.rep：指定次数复制一个字符串
+-- end
+
+foo=loadstring("local a=10;print(a+10)")
+foo()
+
+foo2=loadfile("compileTest.lua")    --完成编译
+
+print(foo1) --nill
+foo2()  --定义
+foo1("ok")  --ok
