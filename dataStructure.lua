fab902a40cca1dfdc868df1c7717a6253e2db57f
HysenChan <HysenChan@163.com>
2020-08-28 17:18:03 +0800

    【添加】[数据结构]图的使用

--- dataStructure.lua
+++ dataStructure.lua
@@ -142,6 +142,7 @@ end
 
 ]]
 
+--[[
 --字符串缓冲
 --Java/C#的StringBuffer
 -- local buff=""
@@ -155,3 +156,62 @@ for line in io.lines() do
 end
 local s=table.concat(t,"\n").."\n"
 print(s)
+]]
+
+--图
+local function name2node(graph,name)
+    if not graph[name] then
+        --节点不存在，创建一个新的
+        graph[name]={name=name,adj={}}
+    end
+    return graph[name]
+end
+
+function readgraph()
+    local graph={}
+    for line in io.lines() do
+        --切分行中的两个名称
+        local namefrom,nameto=string.match(line,"(%S+)%s=(%S+)")
+        --查找相应的结点
+        local from=name2node(graph,namefrom)
+        local to=name2node(graph,nameto)
+        --将'to'添加到'from'的邻接集合
+        from.adj[to]=true
+    end
+    return graph
+end
+
+function findpath(curr,to,path,visited)
+    path=path or {}
+    visited=visited or {}
+    if visited[curr] then   --结点是否已访问过？
+        return nil  --这里没有路径
+    end
+    visited[curr]=true  --将结点标记为已访问过
+    path[#path+1] = curr    --将其加到路径中
+    if curr==to then    --最后的结点吗
+        return path
+    end
+    --尝试所有的邻接节点
+    for node in pairs(curr.adj) do
+        local p=findpath(node,to,path,visited)
+        if p then
+            return p
+        end
+    end
+    path[#path]=nil --从路径中删除结点
+end
+
+function printpath(path)
+    for i = 1, #path do
+        print(path[i].name)
+    end
+end
+
+g=readgraph()
+a=name2node(g,"a")
+b=name2node(g,"b")
+p=findpath(a,b)
+if p then
+    printpath(p)
+end
