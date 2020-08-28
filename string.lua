fea6acadd807b694e2a14d08bde41cdcc0f0ac79
HysenChan <HysenChan@163.com>
2020-08-28 11:42:59 +0800

    【添加】[string]调试string.gsub

--- string.lua
+++ string.lua
@@ -1,3 +1,4 @@
+--[[
 a="one string"
 b=string.gsub(a,"one","another")
 print(a)
@@ -18,3 +19,12 @@ print(10 .. ""=="10")   -->true
 print(a)
 print(#a)
 print(#"good\0bye")
+]]
+
+
+s="hello world world wrold from lua"
+for w in string.gmatch(s,"%a+") do
+    print(w)
+end
+
+print(string.gsub(s,"world","hysen",2))
