be6bf5bc2c422aeae0dab7f05ab24787f961b252
HysenChan <HysenChan@163.com>
2020-08-28 14:56:39 +0800

    【添加】[数据结构]矩阵和多维数组的使用

--- dataStructure.lua
+++ dataStructure.lua
@@ -1,5 +1,5 @@
 --第十一章——数据结构
-
+--[[
 --基本数组的创建
 a={}
 for i = -5, 5 do
@@ -12,3 +12,32 @@ end
 
 squares={1,4,9,16,25,36,49,64,81}
 print(#squares)
+]]
+
+--矩阵与多维数组
+--NxM的零矩阵
+--方式1：
+mt={}   --创建矩阵
+for i = 1, N do
+    mt[i]={}    --创建一个新行
+    for j = 1, M do
+        mt[i][j]=0
+    end
+end
+
+--方式2：
+mt={}   --创建矩阵
+for i = 1, N do
+    mt[i]={}    --创建一个新行
+    for j = 1, M do
+        mt[(i-1)*M+j]=0
+    end
+end
+
+function mult(a,rowindex,k)
+    local row=a[rowindex]   --a这个table中的rowindex行的table
+    for i,v in pairs(row) do
+        row[i]=v*k  --每行都乘以一个k
+    end
+end
+
