32ffca7be1be76bbda25b023b3ad909421967497
HysenChan <HysenChan@163.com>
2020-08-28 15:59:01 +0800

    【添加】[数据结构]队列的实现

--- dataStructure.lua
+++ dataStructure.lua
@@ -42,6 +42,7 @@ function mult(a,rowindex,k)
 end
 ]]
 
+--[[
 --链表
 list=nil    --列表头结点的变量
 list={next=list,value=v}    --在表头插入一个元素，元素值为v
@@ -51,4 +52,55 @@ while l do
     print(l.value)
     l=l.next
 end
+]]
+
+--队列与双向队列
+--方式1：
+-- function ListNew()
+--     return {first=0,last=-1}
+-- end
+
+--方式2：
+List={}
+function List.new()
+    return {first=0,last=-1}
+end
+
+function List.pushFirst(list,value)
+    local first=list.first-1
+    list.first=first
+    list[first]=value
+end
+
+function List.pushLast(list,value)
+    local last=list.last+1
+    list.last=last
+    list[last]=value
+end
+
+function List.popFirst(list)
+    local first=list.first
+    if first>list.last then
+        error("list is empty")
+    end
+    local value=list[first]
+    list[first]=nil --为了允许垃圾收集
+    list.first=first+1
+    return value
+end
+
+function List.popLast(list)
+    local last=list.last
+    if list.first>last then
+        error("list is empty")
+    end
+    local value=list[last]
+    list[last]=nil  --为了允许垃圾收集
+    list.last=last-1
+    return value;
+end
+
+a=List.new()
+List.pushFirst(a,5)
+List.pushLast(a,10)
 
