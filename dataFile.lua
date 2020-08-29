dfb9cb4225a01e958c4e3293c01481726981da5f
HysenChan <HysenChan@163.com>
2020-08-29 12:49:22 +0800

    【添加】[数据文件]串行化

--- dataFile.lua
+++ dataFile.lua
@@ -1,5 +1,6 @@
 --第十二章——数据文件
 
+--[[
 local count=0
 function Entry(_)
     count=count+1
@@ -18,3 +19,13 @@ for name in pairs(authors) do
     print(name)
 end
 
+]]
+
+--串行化
+function serialize(o)
+    if type(o)=="number" then   --对于一个数字值，如果是字符串值：type(o)=="string"
+        io.write(o)
+    else
+        print("nil")
+    end
+end
