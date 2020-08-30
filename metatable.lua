3a29698a32aabfb74403e42bca04c2963633a0c2
HysenChan <HysenChan@163.com>
2020-08-30 19:11:20 +0800

    【添加】[元数据]关系类的元方法

--- metatable.lua
+++ metatable.lua
@@ -13,15 +13,10 @@ print(getmetatable(10))
 --算术类的元方法
 Set={}
 
---根据参数列表中的值创建一个新的集合
-function Set.new(l)
-    local set={}
-    for _,v in ipairs(l) do
-        set[v]=true
-    end
-end
-
 function Set.union(a,b)
+    -- if getmetatable(a)~=mt or getmetatable(b)~=mt then
+    --     error("attempt to 'add' a set with a non-set value",2)
+    -- end
     local res=Set.new{}
     for k in pairs(a) do
         res[k]=true
@@ -29,6 +24,7 @@ function Set.union(a,b)
     for k in pairs(b) do
         res[k]=true
     end
+    return res
 end
 
 function Set.intersection(a,b)
@@ -44,7 +40,7 @@ function Set.tostring(set)
     for e in pairs(set) do
         l[#l+1]=e
     end
-    return "{"..table.concat(l,",").."}"
+    return "{" .. table.concat(l,",") .. "}"
 end
 
 function Set.print(s)
@@ -53,4 +49,24 @@ end
 
 local mt={} --集合的元表
 
+--根据参数列表中的值创建一个新的集合
+function Set.new(l)
+    local set={}
+    setmetatable(set,mt)
+    for _,v in ipairs(l) do
+        set[v]=true
+    end
+    return set
+end
 
+s1=Set.new{10,20,30,50}
+s2=Set.new{30,1}
+print(getmetatable(s1))
+print(getmetatable(s2))
+
+mt.__add=Set.union
+mt.__mul=Set.intersection
+
+s3=s1+s2
+Set.print(s3)    --s1和s2的并集 1,20,30,10,50
+Set.print(s3*s1)   --s3和s1的交集 20,50,30,10
