76e1f2ec99041dc1c6c3c08c531f5568086d958f
HysenChan <HysenChan@163.com>
2020-08-30 17:39:05 +0800

    【添加】[数据文件]字符串值输出的使用

--- dataFile.lua
+++ dataFile.lua
@@ -25,7 +25,25 @@ end
 function serialize(o)
     if type(o)=="number" then   --对于一个数字值，如果是字符串值：type(o)=="string"
         io.write(o)
+    elseif type(o)=="string" then
+        io.write(string.format("%q",o))
     else
         print("nil")
     end
+end
+
+a='a "problematic" \\string'
+serialize(a)
+
+function quote(s)
+    --查找最长的等号序列
+    local n=-1
+    for w in string.gmatch(s,"]=*") do
+        n=math.max(n,#w-1)
+    end
+    --产生'n'+1个等号
+    local eq=string.rep("=",n+1)
+
+    --生成长字符串的字面表示
+    return string.format(" [&s[\n%s]%s ",eq,s,eq)
 end
