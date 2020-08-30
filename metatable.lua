8396c14ce0c89109388bb72848d0868de0c96af7
HysenChan <HysenChan@163.com>
2020-08-30 18:20:22 +0800

    【添加】[元数据]基础的Set.*操作

--- metatable.lua
+++ metatable.lua
@@ -9,3 +9,48 @@ assert(getmetatable(t)==t1)
 
 print(getmetatable("hi"))
 print(getmetatable(10))
+
+--算术类的元方法
+Set={}
+
+--根据参数列表中的值创建一个新的集合
+function Set.new(l)
+    local set={}
+    for _,v in ipairs(l) do
+        set[v]=true
+    end
+end
+
+function Set.union(a,b)
+    local res=Set.new{}
+    for k in pairs(a) do
+        res[k]=true
+    end
+    for k in pairs(b) do
+        res[k]=true
+    end
+end
+
+function Set.intersection(a,b)
+    local res=Set.new{}
+    for k in pairs(a) do
+        res[k]=b[k]
+    end
+    return res
+end
+
+function Set.tostring(set)
+    local l={}  --用于存放集合中所有元素的列表
+    for e in pairs(set) do
+        l[#l+1]=e
+    end
+    return "{"..table.concat(l,",").."}"
+end
+
+function Set.print(s)
+    print(Set.tostring(s))
+end
+
+local mt={} --集合的元表
+
+
